09262ab2ac1668899bd6f7da5fd6489c1344c380
Mr.Coxall@mths.ca at codea-scm <Mr.Coxall@mths.ca@codea-scm.aws.mapote.com>
2015-09-29 22:38:24 -0400

    First commit

---
+++ tabs/Main.lua
@@ -0,0 +1,37 @@
+-- Lesson3
+
+-- Created by: Mr Coxall
+-- Created on: Nov 2013
+-- Created for: ICS2O
+-- This program displays an image and moves it around the screen
+
+-- variables local to entire file
+local manPosition = vec2()
+
+-- Use this function to perform your initial setup
+function setup()
+    -- setup display and drawing functions
+    supportedOrientations(LANDSCAPE_ANY)    
+    displayMode(FULLSCREEN)
+    noFill()
+    noSmooth()
+    noStroke()
+    pushStyle()
+    
+    manPosition = vec2(WIDTH/2, HEIGHT/2)
+end
+
+-- This function gets called once every frame
+function draw()
+    
+    -- local varaibles
+ 
+    manPosition.x = manPosition.x + (Gravity.x * 10)
+    -- This sets a dark background color 
+    background(0, 0, 0, 255)
+    
+    sprite("SpaceCute:Background", 512, 384, 1024, 768)
+    sprite("SpaceCute:Beetle Ship", manPosition.x, manPosition.y)
+end
+
+
